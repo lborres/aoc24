885b0ceb85acd424e49ae0673c96bfbf017a59c6
Louis Borres <lborresbusiness@gmail.com>
2024-12-07 18:39:41 +0800

    feat: solution to day 01 part 02

--- process/dist.go
+++ process/dist.go
@@ -13,17 +13,62 @@ import (
 	"github.com/lborres/aoc24/internal/calc"
 )
 
-func totalDistance(arrA []int, arrB []int) ( int, error ){
+// similarityScore() takes in 2 sorted arrays
+// prints out similarity score as described by the problem
+func similarityScore(arrA []int, arrB []int) error {
+  result := 0
+  ptrA := 0
+  ptrB := 0
+  sim := 0
+  fmt.Printf("Checking Similarity for the following:\nLeft: %v\nRight: %v\n", arrA, arrB)
+
+  // sample inputs
+  // Left: [1 2 3 3 3 4]
+  // Right: [3 3 3 4 5 9]
+  for ptrA <= len(arrA) - 1 {
+    fmt.Printf("%vx%v: %v\n", arrA[ptrA], arrB[ptrB], result)
+
+    // INFO: If current value is same as previous, use the same similarity val
+    if ptrA > 0 && arrA[ptrA] == arrA[ptrA - 1] {
+      result = result + arrA[ptrA] * sim
+      ptrA++
+      continue
+    }
+
+    sim = 0
+    for arrA[ptrA] == arrB[ptrB] && ptrB <= len(arrB) - 1 {
+      sim++
+      ptrB++
+    }
+    result += arrA[ptrA] * sim
+
+    // INFO: Traverse arrB until we can start comparing
+    if arrA[ptrA] > arrB[ptrB] {
+      ptrB++
+      continue
+    }
+    ptrA++
+  }
+
+
+  fmt.Printf("Similarity Score: %d\n",result)
+
+  return nil
+}
+
+func totalDistance(arrA []int, arrB []int) error {
   result := 0
 
   for i := 0; i <= len(arrA) - 1; i++ {
     result += calc.AbsInt(arrA[i] - arrB[i])
   }
 
-  return result, nil
+  fmt.Printf("Total Distance: %d\n",result)
+
+  return nil
 }
 
-// CheckDist is the main process for the solution to day 01 part 01
+// CheckDist is the main process for the solution to day 01
 func CheckDist(inputFile string) {
   file, err := os.Open(inputFile)
   if err != nil {
@@ -82,10 +127,13 @@ func CheckDist(inputFile string) {
     log.Fatalln(err)
   }
 
-  // NOTE: Final output
-  result, err := totalDistance(sortedA, sortedB)
-  if err != nil {
+  // INFO: Final output
+  // Solution to Part 1
+  if err := totalDistance(sortedA, sortedB); err != nil {
     log.Fatalln(err)
   }
-  fmt.Printf("%d\n",result)
+  if err := similarityScore(sortedA, sortedB); err != nil {
+    log.Fatalln(err)
+  }
+
 }
